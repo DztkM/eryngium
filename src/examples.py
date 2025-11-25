729b0f02d4db01754c398ea46e7a48a47b108a13
Maksim Dziatkou <dziatkou.maksim@gmail.com>
2025-11-25 13:14:06 +0100

    feat: add examples for seird and seiard models with lockdown intervention

--- src/examples.py
+++ src/examples.py
@@ -5,8 +5,9 @@ from seird.config_seird import ConfigSEIRD
 from seird.abm_network_seird import ABMNetworkSEIRD
 from seiard.config_seiard import ConfigSEIARD
 from seiard.abm_network_seiard import ABMNetworkSEIARD
-from visualization import plot_sir, plot_seird, plot_seiard, plot_network, animate_network_spread
+from visualization import plot_history, plot_network, animate_network_spread
 from intervention.interventions_examples import Lockdown
+
 
 def ex_sir_1():
     cfg = Config(
@@ -16,7 +17,9 @@ def ex_sir_1():
 
     # Run model
     model.run(days=90)
-    plot_sir(model.history)
+    fig = plot_history(model.history, "SIR")
+    fig.savefig("img/sir.png", dpi=300)
+
 
 def ex_sirnetwork_1():
     cfg = Config(
@@ -27,18 +30,19 @@ def ex_sirnetwork_1():
     # Run model
     model_net.run(days=60)
 
-    fig = plot_sir(model_net.history)
-    fig.savefig("sir_network.png", dpi=300)
+    fig = plot_history(model_net.history, "SIR_network")
+    fig.savefig("img/sir_network.png", dpi=300)
 
-    # Plot final network state
-    agent_states = [a.state for a in model_net.agents]
-    plot_network(model_net.G, agent_states)
+    # # Plot final network state
+    # agent_states = [a.state for a in model_net.agents]
+    # plot_network(model_net.G, agent_states)
 
+    # Animate infection spread:
     ani = animate_network_spread(model_net, interval=200)
-    ani.save("sir_network_spread.gif", writer="pillow", fps=1)
+    ani.save("img/sir_network_spread.gif", writer="pillow", fps=1)
 
 
-def ex_seird_network_1():
+def ex_seird_1():
     cfg = ConfigSEIRD(
         N=1500,
         I0=50,
@@ -46,39 +50,36 @@ def ex_seird_network_1():
         seed=42,
     )
     model_seird = ABMNetworkSEIRD(cfg, network_type="watts_strogatz", k=10, beta=0.1,)
+
     # Run model
     model_seird.run(days=64)
 
-    plot_seird(model_seird.history)
+    fig = plot_history(model_seird.history, "SEIRD")
+    fig.savefig("img/seird.png", dpi=300)
 
-    # Plot final network state
-    agent_states = [a.state for a in model_seird.agents]
-    plot_network(model_seird.G, agent_states, model_type="SEIRD")
-
+    # Animate infection spread:
     ani = animate_network_spread(model_seird, interval=200, model_type="SEIRD")
-    ani.save("seird_network_spread.gif", writer="pillow", fps=1)
+    ani.save("img/seird_spread.gif", writer="pillow", fps=1)
 
 
-def ex_seiard_network_1():
+def ex_seiard_1():
     cfg = ConfigSEIARD(
         N=1500,
         I0=50,
         contacts_per_day=15, 
         seed=42,
     )
+
     model_seiard = ABMNetworkSEIARD(cfg, network_type="watts_strogatz", k=10, beta=0.1,)
     # Run model
     model_seiard.run(days=64)
 
+    fig = plot_history(model_seiard.history, "SEIARD")
+    fig.savefig("img/seiard.png", dpi=300)
 
-    plot_seiard(model_seiard.history)
-
-    # Plot final network state
-    agent_states = [a.state for a in model_seiard.agents]
-    plot_network(model_seiard.G, agent_states, model_type="SEIARD")
-
+    # Animate infection spread:
     ani = animate_network_spread(model_seiard, interval=200, model_type="SEIARD")
-    ani.save("seiard_network_spread.gif", writer="pillow", fps=1)
+    ani.save("img/seiard_spread.gif", writer="pillow", fps=1)
 
 
 def ex_interventions_sirnetwork_1():
@@ -93,15 +94,56 @@ def ex_interventions_sirnetwork_1():
     # Run model
     model_net.run(days=60)
 
-    fig = plot_sir(model_net.history)
-    fig.savefig("interventions_sir_network.png", dpi=300)
-
-    # Plot final network state
-    agent_states = [a.state for a in model_net.agents]
-    plot_network(model_net.G, agent_states)
-
-    ani = animate_network_spread(model_net, interval=200)
-    ani.save("interventions_sir_network_spread.gif", writer="pillow", fps=1)
-
-
+    fig = plot_history(model_net.history, "SIR_network (+lockdown)")
+    fig.savefig("img/interventions_sir_network.png", dpi=300)
     
+    # Animate infection spread:
+    ani = animate_network_spread(model_net, interval=200)
+    ani.save("img/interventions_sir_network_spread.gif", writer="pillow", fps=1)
+
+
+def ex_interventions_seird_1():
+    cfg = ConfigSEIRD(
+        N=1500,
+        I0=50,
+        contacts_per_day=15, 
+        seed=42,
+    )
+    interventions = [
+            Lockdown(start_day=8, end_day=15, reduction_factor=0.5),
+        ]
+    
+    model_seird = ABMNetworkSEIRD(cfg, interventions=interventions, network_type="watts_strogatz", k=10, beta=0.1,)
+    # Run model
+    model_seird.run(days=64)
+
+    fig = plot_history(model_seird.history, "SEIRD (+lockdown)")
+    fig.savefig("img/interventions_seird.png", dpi=300)
+
+    # Animate infection spread:
+    ani = animate_network_spread(model_seird, interval=200, model_type="SEIRD")
+    ani.save("img/interventions_seird_spread.gif", writer="pillow", fps=1)
+
+
+def ex_interventions_seiard_1():
+    cfg = ConfigSEIARD(
+        N=1500,
+        I0=50,
+        contacts_per_day=15, 
+        seed=42,
+    )
+    interventions = [
+            Lockdown(start_day=8, end_day=15, reduction_factor=0.5),
+        ]
+    
+    model_seiard = ABMNetworkSEIARD(cfg, interventions=interventions, network_type="watts_strogatz", k=10, beta=0.1,)
+    # Run model
+    model_seiard.run(days=64)
+
+    # plot_seiard(model_seiard.history)
+    fig = plot_history(model_seiard.history, "SEIARD (+lockdown)")
+    fig.savefig("img/interventions_seiard.png", dpi=300)
+
+    # Animate infection spread:
+    ani = animate_network_spread(model_seiard, interval=200, model_type="SEIARD")
+    ani.save("img/interventions_seiard_spread.gif", writer="pillow", fps=1)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from examples import ex_sir_1, ex_sirnetwork_1, ex_seird_network_1, ex_seiard_network_1, ex_interventions_sirnetwork_1
+from examples import ex_sir_1, ex_sirnetwork_1, ex_seird_1, ex_seiard_1
+from examples import ex_interventions_sirnetwork_1, ex_interventions_seird_1, ex_interventions_seiard_1
+
 def main():
-    # ex_sir_1()
+    ex_sir_1()
     ex_sirnetwork_1()
-    # ex_seird_network_1()
-    # ex_seiard_network_1()
+    ex_seird_1()
+    ex_seiard_1()
     ex_interventions_sirnetwork_1()
+    ex_interventions_seird_1()
+    ex_interventions_seiard_1()
 
 
 if __name__ == "__main__":
